c0aeca03e1d3d3f203a6fc87e59deb7e17463960
lbirts <birtslauren@gmail.com>
2020-05-11 15:38:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Lauren!"
+    end
+
+    get '/hometown' do
+        "My hometown is Orlando!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Chicago Freestyle!"
+    end
+
+
+
 end
